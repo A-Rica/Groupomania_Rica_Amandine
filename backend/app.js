2c636b159ddb761e2cc71b94d103cfd72b1115f3
A-Rica <91880772+A-Rica@users.noreply.github.com>
2022-03-06 11:31:55 +0100

    importation des images logo groupomania

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+  //requêtes Cors (cross origin ressource Sharing)//
+  app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   res.setHeader('Content-Security-Policy', "default-src 'self'");
+   next();
+ });
+
+
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
